4811cb3d08c56e27fe7772d9e1af2532ae8dafcb
arunasesay034 <arunasesay034@gmail.com>
2026-05-05 14:21:12 +0000

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,10 @@
+score = float(input("Enter first score: "))
+score = float(input("Enter second score: "))
+score = float(input("Enter third score: "))
+
+average = (score1 + score2 + score3) / 3
+
+print("average:", average)
+
+except:
+    print("Invalid input! please enter numbers only.")
---
+++ Purchase.py
@@ -0,0 +1,4 @@
+name = input("Enter your name")
+item = input("Enter the item you purchase")
+
+print(f"welcome, {name}! You have purchase a {item}")
